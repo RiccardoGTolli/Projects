be268911b9c9e13498503f2d06f0bc6bd38fa9b3
Riccardo <rikkardeye@gmail.com>
2019-03-20 22:19:59 +0000

    Third Commit

--- Big Data Applications/Assignment2_v2_spark_v2.R
+++ Big Data Applications/Assignment2_v2_spark_v2.R
@@ -17,19 +17,6 @@ DF.spark <-as.DataFrame(DF)
 
 head(DF.spark)
 
-# There are warnings saying that the task I am doing is too big  
-# e.g.->"18/03/28 19:10:53 WARN scheduler.TaskSetManager: Stage 2 contains a task of very large size (27224 KB). The maximum recommended task size is 100 KB."
-
-
-# The operation on sparkR 2.1.0 on the cluster are unbelieavly slow, I
-# am trying to solve this by running parallel processing with the library
-# "parallel"
-
-
-# If I were to use spark R on apache spark 1.4.0 on the cluster (which seems faster)
-# I wouldnt be able to go past  creating  the dataframe as NO machine learning
-# algorithms are available on 1.4.0
-
 
 # Split the  dataset into train and test 
 trainTest <-randomSplit(DF.spark,c(0.7,0.3), 123)
@@ -42,19 +29,6 @@ head(select(train, train$First_Blood))
 # Logistic regression predicting the winner using First Blood as predictor
 spark.glm.train <- spark.logit(train, Winner ~ First_Blood, maxIter = 10, regParam = 0.3, elasticNetParam = 0.8)
 
-# getting warnings of this kind :
-#"spark.glm.train <- spark.logit(train, Winner ~ First_Blood, maxIter = 10, regP18/03/28 20:56:51 WARN scheduler.TaskSetManager: Stage 3 contains a task of very large size (27224 KB). The maximum recommended task size is 100 KB.
-# 18/03/28 20:57:51 WARN scheduler.TaskSetManager: Stage 5 contains a task of very large size (27224 KB). The maximum recommended task size is 100 KB.
-# 18/03/28 20:58:50 WARN scheduler.TaskSetManager: Stage 6 contains a task of very large size (27224 KB). The maximum recommended task size is 100 KB.
-# 18/03/28 20:58:50 WARN netlib.BLAS: Failed to load implementation from: com.github.fommil.netlib.NativeSystemBLAS
-# 18/03/28 20:58:50 WARN netlib.BLAS: Failed to load implementation from: com.github.fommil.netlib.NativeRefBLAS"
-
-
-# With this same exact code at the beginning I was able to run it with no netlib.BLAS warnings and the code was running correctly but 
-# at some point these errors started popping up and when I do the summary for the models all the coefficients are zero
-
-# I am not sure if this is on my end or cluster's end because I am following the sparkR 2.1.0 documentation very closely
-
 
 # Model summary
 summary(spark.glm.train)
@@ -62,9 +36,6 @@ summary(spark.glm.train)
 # Prediction
 glm.train.probs <- predict(spark.glm.train, test)
 head(glm.train.probs)
-
-
-
 
 
 
